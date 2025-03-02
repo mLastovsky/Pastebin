47f977511bd02fd56b200515da6fa832b16fe9d6
mLastovsky <LastovskyTech@gmail.com>
2025-03-02 04:40:43 +0300

    initialized pastebin-api-service

---
+++ pastebin-api-service/src/main/java/com/mlastovsky/PastebinApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mlastovsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinApiServiceApplication.class, args);
+    }
+
+}
---
+++ pastebin-api-service/src/test/java/com/mlastovsky/PastebinApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlastovsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
