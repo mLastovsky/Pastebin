ce3c6000e076af922b2ec2bd1927e2a96cfdc167
mLastovsky <LastovskyTech@gmail.com>
2025-03-30 00:32:36 +0300

    initialize config-server service

---
+++ services/config-server/src/test/java/com/mlastovsky/pastebin/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlastovsky.pastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
