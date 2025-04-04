04110f51715d7ced6b1fafeef308cf0f2bbea46c
mLastovsky <LastovskyTech@gmail.com>
2025-04-04 11:17:20 +0300

    initialize pastebin-api-service

---
+++ services/pastebin-api/src/main/java/com/mlastovsky/pastebin/PastebinApiApplication.java
@@ -0,0 +1,13 @@
+package com.mlastovsky.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastebinApiApplication.class, args);
+	}
+
+}
